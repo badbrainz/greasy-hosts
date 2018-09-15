367922c7b518843c2a2db30be32b7ece1c6df118
badbrainz <wormboy.d@gmail.com>
2018-09-14 20:02:36 -0700

    Step by step install

--- install.js
+++ install.js
@@ -3,10 +3,10 @@
 const fs = require('fs')
 const path = require('path')
 const util = require('util')
+const readline = require('readline')
 const writeFile = util.promisify(fs.writeFile)
 
 const args = {
-  browser: 'firefox',
   root: false
 }
 for (const arg of process.argv.slice(2)) {
@@ -17,103 +17,131 @@ for (const arg of process.argv.slice(2)) {
 const HOST = 'io.greasyhost.%NAME%'
 
 // Firefox system-wide
-const TARGET_DIR_FF_LNX_SW = '/usr/lib/mozilla/native-messaging-hosts'
-const TARGET_DIR_FF_OSX_SW = '/Library/Application Support/Mozilla/NativeMessagingHosts'
+const TARGET_FF_LNX_SW = '/usr/lib/mozilla/native-messaging-hosts'
+const TARGET_FF_OSX_SW = '/Library/Application Support/Mozilla/NativeMessagingHosts'
 // Firefox user-specific
-const TARGET_DIR_FF_LNX = path.join(process.env.HOME, '.mozilla/native-messaging-hosts')
-const TARGET_DIR_FF_OSX = path.join(process.env.HOME, 'Library/Application Support/Mozilla/NativeMessagingHosts/')
+const TARGET_FF_LNX = path.join(process.env.HOME, '.mozilla/native-messaging-hosts')
+const TARGET_FF_OSX = path.join(process.env.HOME, 'Library/Application Support/Mozilla/NativeMessagingHosts/')
 // Chrome system-wide
-const TARGET_DIR_CR_LNX_SW = '/etc/opt/chrome/native-messaging-hosts'
-const TARGET_DIR_CR_OSX_SW = '/Library/Google/Chrome/NativeMessagingHosts'
+const TARGET_CR_LNX_SW = '/etc/opt/chrome/native-messaging-hosts'
+const TARGET_CR_OSX_SW = '/Library/Google/Chrome/NativeMessagingHosts'
 // Chrome user-specific
-const TARGET_DIR_CR_LNX = path.join(process.env.HOME, '.config/google-chrome/NativeMessagingHosts')
-const TARGET_DIR_CR_OSX = path.join(process.env.HOME, 'Library/Application Support/Google/Chrome/NativeMessagingHosts')
+const TARGET_CR_LNX = path.join(process.env.HOME, '.config/google-chrome/NativeMessagingHosts')
+const TARGET_CR_OSX = path.join(process.env.HOME, 'Library/Application Support/Google/Chrome/NativeMessagingHosts')
 
-let FIREFOX
-let CHROME
-switch (args.browser) {
-  case 'firefox':
-    FIREFOX = true
-    break;
-  case 'chrome':
-    CHROME = true
-    break;
-  default:
-    console.error('%s not supported', args.browser)
-    process.exit(1)
-}
+const WIN_REG_FF_LM = 'HKLM\\SOFTWARE\\Mozilla\\NativeMessagingHosts\\'
+const WIN_REG_FF_CU = 'HKCU\\SOFTWARE\\Mozilla\\NativeMessagingHosts\\'
+const WIN_REG_CR_LM = 'HKLM\\SOFTWARE\\Google\\Chrome\\NativeMessagingHosts\\'
+const WIN_REG_CR_CU = 'HKCU\\SOFTWARE\\Google\\Chrome\\NativeMessagingHosts\\'
 
-let TARGET_DIR
+let TARGET_FF
+let TARGET_CR
+let WIN_REG_FF
+let WIN_REG_CR
 switch (process.platform) {
   case 'linux':
     if (args.root) {
-      TARGET_DIR = FIREFOX ? TARGET_DIR_FF_LNX_SW : TARGET_DIR_CR_LNX_SW
+      TARGET_FF = TARGET_FF_LNX_SW
+      TARGET_CR = TARGET_CR_LNX_SW
     } else {
-      TARGET_DIR = FIREFOX ? TARGET_DIR_FF_LNX : TARGET_DIR_CR_LNX
+      TARGET_FF = TARGET_FF_LNX
+      TARGET_CR = TARGET_CR_LNX
     }
     break;
   case 'darwin':
     if (args.root) {
-      TARGET_DIR = FIREFOX ? TARGET_DIR_FF_OSX_SW : TARGET_DIR_CR_OSX_SW
+      TARGET_FF = TARGET_FF_OSX_SW
+      TARGET_CR = TARGET_CR_OSX_SW
     } else {
-      TARGET_DIR = FIREFOX ? TARGET_DIR_FF_OSX : TARGET_DIR_CR_OSX
+      TARGET_FF = TARGET_FF_OSX
+      TARGET_CR = TARGET_CR_OSX
     }
     break;
   case 'win32':
-    TARGET_DIR = __dirname
+    TARGET_FF = TARGET_CR = __dirname
+    WIN_REG_FF = args.root ? WIN_REG_FF_LM : WIN_REG_FF_CU
+    WIN_REG_CR = args.root ? WIN_REG_CR_LM : WIN_REG_CR_CU
     break;
   default:
     console.error('%s not supported', process.platform)
     process.exit(1)
 }
 
-const files = []
+const scripts_dir = path.resolve(__dirname, 'user_scripts')
 
-if (FIREFOX) {
-  console.log('Installing for Firefox:', TARGET_DIR)
-  files.push(install(TARGET_DIR, {
-    allowed_extensions: [
-      '{e4a8a97b-f2ed-450b-b12d-ee082ba24781}'
-    ]
-  }))
+const ff_whitelist = {
+  allowed_origins: [
+    'chrome-extension://daihojmdjmhocgfjnhifkpdkdjaikjoj'
+  ]
 }
 
-if (CHROME) {
-  console.log('Installing for Chrome:', TARGET_DIR)
-  files.push(install(TARGET_DIR, {
-    allowed_origins: [
-      'chrome-extension://daihojmdjmhocgfjnhifkpdkdjaikjoj'
-    ]
-  }))
+const cr_whitelist = {
+  allowed_extensions: [
+    '{e4a8a97b-f2ed-450b-b12d-ee082ba24781}'
+  ]
 }
 
-Promise.all(files).catch(e => console.error('%s', e))
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
 
-async function install(target, whitelist) {
+new Promise((resolve, reject) => {
+  rl.question('Install for Firefox? y/n ', (input) => {
+    input = input.trim()
+    if (input == 'y' || !input) {
+      install(TARGET_FF, ff_whitelist, WIN_REG_FF).then(resolve).catch(reject)
+    } else {
+      resolve()
+    }
+  })
+}).then(() => new Promise((resolve, reject) => {
+  rl.question('Install for Chrome? y/n ', (input) => {
+    input = input.trim()
+    if (input == 'y' || !input) {
+      install(TARGET_CR, cr_whitelist, WIN_REG_CR).then(resolve).catch(reject)
+    } else {
+      resolve()
+    }
+  })
+})).then(() => new Promise((resolve, reject) => {
+  fs.mkdir(scripts_dir, 0744, (err) => {
+    if (err) {
+      if (err.code == 'EEXIST') {
+        resolve()
+      } else {
+        reject(err)
+      }
+    } else {
+      resolve()
+    }
+  })
+})).catch((e) => {
+  console.error('%s', e)
+}).finally(() => {
+  rl.close()
+})
+
+async function install(target, whitelist, key) {
   for (const name of 'read write watch spawn'.split(' ')) {
-    const host = HOST.replace('%NAME%', name)
-    const app = path.resolve('bin', `${name}.js`)
-    const manifest = path.join(target, `${host}.json`)
+    const hostname = HOST.replace('%NAME%', name)
+    const script = path.resolve('bin', `${name}.js`)
+    const json = path.join(target, `${hostname}.json`)
 
-    await writeFile(manifest, JSON.stringify({
-      name: host,
-      path: app,
+    await writeFile(json, JSON.stringify({
+      name: hostname,
+      path: script,
       description: 'external editor',
       type: 'stdio',
       ...whitelist
     }, null, 2))
 
     if (process.platform == 'win32') {
-      let regKey = args.root ? 'HKLM' : 'HKCU'
-      regKey += FIREFOX ?
-        '\\SOFTWARE\\Mozilla\\NativeMessagingHosts\\' :
-        '\\SOFTWARE\\Google\\Chrome\\NativeMessagingHosts\\'
-      regKey += host
-      const reg = spawn('REG',
-        ['ADD', regKey, '/t', 'REG_SZ', '/d', manifest, '/f'])
       await new Promise(function(res, rej) {
+        const reg = spawn('REG', ['ADD', key, '/t', 'REG_SZ', '/d', json, '/f'])
         reg.on('close', e => e ? rej(e) : res())
       })
     }
   }
+  console.log('installed')
 }
