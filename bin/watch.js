ae8a9fc1fab756393de07541f4b3052c86b637ab
badbrainz <wormboy.d@gmail.com>
2018-10-01 20:22:43 -0700

    send changes to any file

--- bin/watch.js
+++ bin/watch.js
@@ -1,47 +1,34 @@
 #!/usr/bin/env node
 
-// BUG fileWatcher doesn't error check existing watchers properly
-
 const fs = require('fs')
 const path = require('path')
 const util = require('util')
-const fileWatcher = require('filewatcher')
-const { pipeline, Transform } = require('stream')
-const { stdin, stdout, stderr } = process
-const { Input, Output, Parse, Stringify } = require('./protocol.js')
+const { pipeline, PassThrough } = require('stream')
+const { stdout, stderr } = process
+const { Output, Stringify } = require('./protocol.js')
 
 const scripts_dir = path.resolve(__dirname, '..', 'user_scripts')
-const readdir = util.promisify(fs.readdir)
-const watcher = fileWatcher()
+const stat = util.promisify(fs.stat)
+const tasks = new Set()
 
-const transformer = Transform({
-  objectMode: true,
-  transform(chunk, enc, cb) {
-    Promise.resolve(chunk.toString().split('\n'))
-      .then(watchFiles)
-      .catch(e => this.push({ error: e.toString() }))
-      .finally(cb)
+const stream = PassThrough({ objectMode: true })
+
+function check(file) {
+  return new Promise(function(res, rej) {
+    setTimeout(() => stat(file).then(res).catch(rej), 300)
+  })
+}
+
+fs.watch(scripts_dir, function(event, file) {
+  if (!tasks.has(file)) {
+    tasks.add(file)
+    check(path.join(scripts_dir, file))
+      .then(() => stream.emit('data', { file }))
+      .catch(e => stderr.write(`${e}\n`))
+      .finally(() => tasks.delete(file))
   }
 })
 
-function watchFiles(files) {
-  files.map(f => f.trim())
-    .filter(f => f)
-    .map(f => path.join(scripts_dir, f))
-    .forEach(f => watcher.add(f))
-}
-
-watcher.on('change', function(file, stat) {
-  transformer.emit('data', {
-    file: path.basename(file),
-    deleted: !!stat.deleted
-  })
-})
-
-readdir(scripts_dir)
-  .then(watchFiles)
-  .catch(e => stderr.write(`${e}\n`))
-
-pipeline(stdin, Input(), Parse(), transformer, Stringify(), Output(),
+pipeline(stream, Stringify(), Output(),
   e => stderr.write(`${e || 'end of stream'}`))
   .pipe(stdout)
